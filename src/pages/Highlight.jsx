2c39d6b331fae226794f534dd79dfb3ae4bb92f4
burukgetu <burukgetu000@gmail.com>
2024-11-13 21:20:49 -0300

    edited the ui

--- src/pages/Highlight.jsx
+++ src/pages/Highlight.jsx
@@ -4,6 +4,8 @@ import Hls from 'hls.js';
 import { Maximize, Shrink } from 'lucide-react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Lottie from 'react-lottie';
+import animationData from '/src/ball-animation.json';
 
 const Highlight = () => {
   const { title } = useParams();
@@ -14,9 +16,21 @@ const Highlight = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [videoSrc, setVideoSrc] = useState(null);
+  const [isMobile, setIsMobile] = useState(false);
+  const [date, setDate] = useState(false);
 
   const videoRef = useRef(null);
   const [isInnerFullScreen, setIsInnerFullScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const handleMediaQueryChange = () => setIsMobile(mediaQuery.matches);
+    handleMediaQueryChange();
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   useEffect(() => {
     const fetchMatch = async () => {
@@ -24,8 +38,15 @@ const Highlight = () => {
         const response = await axios.get(`${apiUrl}/match/${id}/${title}`);
         const item = response.data;
         setMatch(item)
-        console.log(match)
-        setVideoSrc(item.vidSrc); // Set the video source from the matched item
+        const dateStr = item.createdAt;
+        const date = new Date(dateStr);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        const formattedDate = `${day}/${month}/${year}`;
+        setDate(formattedDate);
+        console.log(formattedDate)
+        setVideoSrc(item.url); // Set the video source from the matched item
       } catch (error) {
         setError('Error fetching the match', error);
       } finally {
@@ -60,16 +81,21 @@ const Highlight = () => {
     setIsInnerFullScreen(!isInnerFullScreen);
   };
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return (
+    <div className="App anim">
+      <Lottie options={{ animationData, loop: true }} height={400} width={400} />
+    </div>
+  );
   if (error) return <p>{error}</p>;
 
+  console.log(match)
   return (
     <>
       <div
         id="player-container"
         style={{
           position: 'relative',
-          width: isInnerFullScreen ? '100vw' : '100%',
+          width: isInnerFullScreen ? '100vw' : isMobile ? "100%" : '60%',
           height: isInnerFullScreen ? '100vh' : 'auto',
           maxWidth: '100%',
           margin: 'auto',
@@ -85,11 +111,13 @@ const Highlight = () => {
           controls
           style={{
             width: isInnerFullScreen ? '100vh' : '100%',
-            height: isInnerFullScreen ? '100vw' : '100%',
+            height: isInnerFullScreen ? '100vw' : 'auto',
             transform: isInnerFullScreen ? 'rotate(90deg)' : 'none',
             objectFit: 'fill',
             transition: 'width 0.3s, height 0.3s, transform 0.3s',
+            marginTop: isMobile ? "0" : "70px"
           }}
+          className='video'
         >
           Your browser does not support the video element.
         </video>
@@ -120,7 +148,7 @@ const Highlight = () => {
             <p>Exit FullScreen</p>
           </div>
         ) : (
-          <div style={{ display: 'flex', gap: '5px', alignItems: 'center', marginTop: '8px' }}>
+          <div style={{ display: 'flex', gap: '5px', alignItems: 'center', marginBottom: '18px' }}>
             <Maximize size={17} />
             <p>Fullscreen</p>
           </div>
@@ -128,10 +156,15 @@ const Highlight = () => {
       </button>
       {/* <div style={{ backgroundColor: 'black', width: '100%', height: '2px', marginTop: "20px" }}></div> */}
       {match && (
-        <>
+        <div className='details'>
           <h1 className='title'>{match.title.replace(/_/g, ' ')}</h1>
-          <h2 className='league'>{match.leagueType.replace(/_/g, ' ')}</h2>
-        </>
+          <div className='league-list'>
+            <h2 className='league'>{match.leagueType.replace(/_/g, ' ')}</h2>
+            <h2 className='league'>{match.teamNames[0].replace(/_/g, ' ')}</h2>
+            <h2 className='league'>{match.teamNames[1].replace("__Highlights_Video","").replace("_"," ")}</h2>
+          </div>
+          <p className='date'>{date}</p>
+        </div>
       )}
     </>
   );
